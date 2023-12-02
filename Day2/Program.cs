496bad254c3b67577d3dd350db00255fcb73ff73
Jillypuff <Puffuctive@gmail.com>
2023-12-02 12:40:16 +0100

    Day 2 complete

---
+++ Day2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Drawing;
+
+string input = File.ReadAllText("../../../Data.txt");
+string[] inputs = input.Split("\n");
+
+int sum = 0;
+
+foreach (string data in inputs)
+{
+    sum += GetPower(data);
+}
+
+Console.WriteLine(sum);
+
+int GetPower(string data)
+{
+    string[] colors = ["red", "green", "blue"];
+    string[] turns = data.Split(';');
+    int redMin = 0;
+    int greenMin = 0;
+    int blueMin = 0;
+
+    int index = 0;
+    foreach (string turn in turns)
+    {
+        foreach (string color in colors)
+        {
+            index = turn.IndexOf(color);
+            if (index != -1)
+            {
+                int cubes = GetNumberOfCubes(color, turn);
+                switch (color)
+                {
+                    case "red":
+                        redMin = Math.Max(redMin, cubes);
+                        break;
+                    case "green":
+                        greenMin = Math.Max(greenMin, cubes);
+                        break;
+                    case "blue":
+                        blueMin = Math.Max(blueMin, cubes);
+                        break;
+                }
+            }
+        }
+    }
+    return redMin * greenMin * blueMin;
+}
+
+int GetNumberOfCubes(string color, string data)
+{
+    data = " " + data;
+    int index = data.IndexOf(color);
+    return Int32.Parse(data[(index - 3)..(index - 1)]);
+}
+
+// Part one stuff
+//bool CheckValidGame(string data)
+//{
+//    string[] colors = ["red", "green", "blue"];
+//    string[] turns = data.Split(';');
+
+//    int index = -1;
+//    foreach (string turn in turns)
+//    {
+//        foreach (string color in colors)
+//        {
+//            index = turn.IndexOf(color);
+//            if (index != -1)
+//            {
+//                if (CheckToManyCubes(color, turn)){
+//                    return false;
+//                }
+//            }
+//        }
+//    }
+//    return true;
+//}
+
+//bool CheckToManyCubes(string color, string data)
+//{
+//    int maxRed = 12;
+//    int maxGreen = 13;
+//    int maxBlue = 14;
+//    int numberOfCubes = GetNumberOfCubes(color, data);
+
+//    return color switch
+//    {
+//        "red" => numberOfCubes > maxRed,
+//        "green" => numberOfCubes > maxGreen,
+//        "blue" => numberOfCubes > maxBlue,
+//        _ => false,
+//    };
+//}
+
+//int GetGameId(string game)
+//{
+//    int index = game.IndexOf(':');
+//    return Int32.Parse(game[4..index]);
+//}
--- README.md
+++ README.md
@@ -7,3 +7,9 @@ but then the second part hit and it ramped up hard.
 I immediatly googled c# string to find out any comparision trick to use and I ended up finding the IndexOf and LastIndexOf methods.
 From there I had to carefully craft the right variables needed and remade the if statements plenty of times until it finally worked.
 While this problem was harder then I expected from a day 1 problem I left the first problem with two new methods in my arsenal of knowledge.
+
+## Day 2:
+
+The second day was another day of indexing strings with alot of trial and error. I am getting better at debugging and changing around numbers
+but I don't feel like I can visuallise indexes in substrings easy yet.
+All in all day 2 felt easier since it builded on my answer and understanding from day 1.
